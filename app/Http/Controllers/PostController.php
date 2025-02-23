280b03e102cb8ba9737ae2fddba9abf50e555c84
rumale0508 <mkamiya1026@gmail.com>
2025-02-23 16:53:14 +0900

    kadai commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = DB::table('posts')->get();
+        return view('posts.index', ['posts' => $posts]);
+    }
+}
